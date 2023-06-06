8271a499b0e99fd0fd4d7968ae130bdad32538a1
KXLAA <kolade.afode@yahoo.com>
2023-06-06 14:22:24 +0100

    feat: fix tests
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"fmt"
+	"html"
 	"net/http"
 	"regexp"
 	"testing"
 )
 
 var (
-	charRxAll = "^[a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]*$"
-	//charRx    = regexp.MustCompile(`<span class="generator__output__text" id="generator__output__text">(.+?)</span>`)
+	charRxAll         = regexp.MustCompile("^[a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]*$")
+	charRx            = regexp.MustCompile(`<span class="generator__output__text" id="generator__output__text">(.+?)</span>`)
+	lowercase10CharRx = regexp.MustCompile(`^[a-z]{10}$`)
+	uppercase10CharRx = regexp.MustCompile(`^[A-Z]{10}$`)
+	numbers10CharRx   = regexp.MustCompile(`^[0-9]{10}$`)
+	special10CharRx   = regexp.MustCompile(`^[!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?]{10}$`)
 )
 
 func TestHome(t *testing.T) {
@@ -19,7 +24,7 @@ func TestHome(t *testing.T) {
 
 	code, _, body := ts.get(t, "/")
 
-	_, err := rxMatch(body, charRxAll)
+	_, err := rxMatch(body, "^[a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]*$")
 
 	if err != nil {
 		t.Errorf("could not find matching random string in body")
@@ -47,47 +52,43 @@ func TestGenerate(t *testing.T) {
 				name:     "generates a random string when no query params are provided",
 				url:      "/generate",
 				wantCode: http.StatusOK,
-				matchRx:  regexp.MustCompile(charRxAll),
-			}, {
-				name:     "generates a random string with a length of 10",
-				url:      "/generate?length=10",
-				wantCode: http.StatusOK,
-				matchRx:  regexp.MustCompile(`^.{10}$`),
+				matchRx:  charRxAll,
 			}, {
 				name:     "generates a random string with a length of 10 and only lowercase characters",
 				url:      "/generate?length=10&lowercase=true",
 				wantCode: http.StatusOK,
-				matchRx:  regexp.MustCompile(`^[a-z]{10}$`),
+				matchRx:  lowercase10CharRx,
 			}, {
 				name:     "generates a random string with a length of 10 and only uppercase characters",
 				url:      "/generate?length=10&uppercase=true",
 				wantCode: http.StatusOK,
-				matchRx:  regexp.MustCompile(`^[A-Z]{10}$`),
+				matchRx:  uppercase10CharRx,
 			}, {
 				name:     "generates a random string with a length of 10 and only numbers",
 				url:      "/generate?length=10&numbers=true",
 				wantCode: http.StatusOK,
-				matchRx:  regexp.MustCompile(`^[0-9]{10}$`),
+				matchRx:  numbers10CharRx,
 			}, {
 				name:     "generates a random string with a length of 10 and only special characters",
 				url:      "/generate?length=10&special=true",
 				wantCode: http.StatusOK,
-				matchRx:  regexp.MustCompile(`^[!@#$%^&*()_+\-=\[\]{};':\"\\|,.<>\/?]{10}$`),
+				matchRx:  special10CharRx,
 			},
 			{
 				name:     "generates a random string with a combination of all config options",
 				url:      "/generate?length=10&lowercase=true&uppercase=true&numbers=true&special=true",
 				wantCode: http.StatusOK,
-				matchRx:  regexp.MustCompile(charRxAll),
+				matchRx:  charRxAll,
 			},
 		}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			code, _, body := ts.get(t, tt.url)
-			char := extractChar(t, body)
-			fmt.Println(char)
 
+			fmt.Println(string(body))
+
+			char := extractChar(t, body)
 			if !tt.matchRx.MatchString(char) {
 				t.Errorf("want %s; got %s", tt.matchRx, char)
 			}
@@ -112,29 +113,11 @@ func rxMatch(body []byte, regexPattern string) (bool, error) {
 }
 
 func extractChar(t *testing.T, body []byte) string {
-	matches, err := findMatchedString(`<span class="generator__output__text" id="generator__output__text">(.+?)</span>`, body)
-	if err != nil {
-		t.Fatalf("could not find random character in body: %s", body)
+	matches := charRx.FindSubmatch(body)
+
+	if len(matches) < 2 {
+		t.Fatalf("no match for %q in body %s", charRx, body)
 	}
 
-	return matches
-}
-
-func findMatchedString(pattern string, input []byte) (string, error) {
-	// Compile the regex pattern
-	regex, err := regexp.Compile(pattern)
-	if err != nil {
-		return "", err
-	}
-
-	// Find the matching content within the input byte slice
-	match := regex.FindSubmatch(input)
-	fmt.Println(match, "match")
-
-	// Check if a match is found
-	if len(match) > 1 {
-		return string(match[1]), nil
-	}
-
-	return "", fmt.Errorf("No match found")
+	return html.UnescapeString(string(matches[1]))
 }
